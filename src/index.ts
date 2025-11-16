23b27b0e8b98993bee6a779646595fe28d82f59e
Gabriel Paz <gabrieltecncontato@gmail.com>
2025-11-16 00:03:58 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
-import { app } from './app'
+import { app } from './app';
 
-app.listen(process.env.PORT || 5000, () => {
-  console.log(`Server on port ${process.env.PORT} \nhttp://localhost:${process.env.PORT}`)
-})
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}/v1`);
+});
